ba016a1cc413af625e5381158703a82d3bf16089
shewit <hailu.shewit47@gmail.com>
2025-10-02 08:56:12 -0400

    files

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -2,6 +2,16 @@ package com.pluralsight;
 
 public class ShoppingList {
     public static void main(String[] args) {
+        System.out.println("Apples");
+        System.out.println("Bananas");
+        System.out.println("Bread");
+        System.out.println("Milk");
+        System.out.println("Eggs");
+        System.out.println("Rice");
+        System.out.println("Chicken");
+        System.out.println("Tomatoes");
+        System.out.println("Potatoes");
+        System.out.println("Cheese");
 
     }
 }
